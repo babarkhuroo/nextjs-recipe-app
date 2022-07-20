137d3119fe3844d3d2eeedfd8d63a991f464b687
babar <systemrockstar96@gmail.com>
2022-07-20 22:56:52 +0530

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+const about = () => {
+  return (
+    <div>
+      <h2>About Page</h2>
+      <Link href='/'>Home Page</Link>
+    </div>
+  )
+}
+
+export default about
