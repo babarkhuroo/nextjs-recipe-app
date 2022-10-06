5222699960ee61cccfa5fa46fb7f1e7a04c7d88a
babar <babbark32@gmail.com>
2022-10-06 16:02:51 +0530

    fixed like error

--- pages/api/handleLike.js
+++ pages/api/handleLike.js
@@ -1,12 +1,13 @@
-import { sanityClient } from '../../lib/sanity'
+import { previewClient } from '../../lib/sanity'
 
 export default async function likeButtonHandler(req, res) {
   const { _id } = JSON.parse(req.body)
-  const data = await sanityClient
+  const data = await previewClient
     .patch(_id)
     .setIfMissing({ likes: 0 })
     .inc({ likes: 1 })
     .commit()
     .catch((error) => console.log(error))
+
   res.status(200).json({ likes: data.likes })
 }
